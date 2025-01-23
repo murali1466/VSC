e309245bff638e2c355d374a3d0596156438cc9f
danger-3003 <naremsumanth@gmail.com>
2025-01-23 19:55:46 +0530

    Added Gallery Secton - pending

--- src/Sections/Gallery.jsx
+++ src/Sections/Gallery.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import Container from "../Components/Container";
+import Video from "../assets/IntroVideo.mp4";
 
 function Gallery() {
-  return (
-    <div>Gallery</div>
-  )
+    const location = useLocation();
+    const galleryRef = useRef(null);
+
+    useEffect(() => {
+        if (location.hash === "#gallery" && galleryRef.current) {
+            galleryRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [location]);
+
+    return (
+        <div className="pt-28 pb-20" ref={galleryRef}>
+            <Container>
+                <div className="text-center font-bold text-3xl md:text-4xl">
+                    <p className="uppercase text-yellow">Stay & Play</p>
+                    <p className="uppercase text-primary">
+                        At Vishwanadh Sports&nbsp;club
+                    </p>
+                </div>
+                <div>
+                    <video
+                        src={Video}
+                        className="w-full h-auto mt-10 rounded-xl md:rounded-2xl shadow-customShadow"
+                        controls
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        preload="auto"
+                        // poster='https://source.unsplash.com/800x600/?sports'
+                    />
+                </div>
+            </Container>
+        </div>
+    );
 }
 
-export default Gallery
+export default Gallery;
