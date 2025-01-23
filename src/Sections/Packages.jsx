be139dbbb69c7811dbf317643ecdccccdf2d7eb4
danger-3003 <naremsumanth@gmail.com>
2025-01-23 14:20:57 +0530

    Added bg Symbols in Package Swiper

--- src/Sections/Packages.jsx
+++ src/Sections/Packages.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import PackageSwiper from "../Components/PackageSwiper/Swiper";
 import Button from "../Components/Buttons/LinkButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { faCar, faBasketball, faSwimmingPool, faBaseballBatBall, faBaseballBall } from "@fortawesome/free-solid-svg-icons";
 
 function Packages() {
     const location = useLocation();
@@ -22,15 +22,15 @@ function Packages() {
         >
             <div className="flex items-center justify-center flex-col">
                 <div className="w-full 2xl:w-[70rem] relative">
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-center relative z-10">
                         <p className="uppercase text-primary font-bold text-center text-3xl sm:text-4xl">
                             Jaw-Dropping Offers!
                         </p>
                     </div>
-                    <div className="mt-6 mb-5">
+                    <div className="mt-6 mb-5 relative z-10">
                         <PackageSwiper />
                     </div>
-                    <div className="flex items-center justify-center">
+                    <div className="flex items-center justify-center relative z-10">
                         <Button
                             content={"Explore Mind-Blowing Offers"}
                             link={
@@ -41,7 +41,11 @@ function Packages() {
                             }
                         />
                     </div>
-                    
+                    <FontAwesomeIcon icon={faBaseballBall} className="text-darkBlue/70 absolute text-[5rem] -top-20"/>
+                    <FontAwesomeIcon icon={faBaseballBatBall} className="text-darkBlue/70 absolute text-3xl right-0 bottom-10"/>
+                    <FontAwesomeIcon icon={faSwimmingPool} className="text-darkBlue/70 absolute text-5xl -bottom-10 left-[50%]"/>
+                    <FontAwesomeIcon icon={faBasketball} className="text-darkBlue/70 absolute text-3xl top-0 right-[20%]"/>
+                    <FontAwesomeIcon icon={faCar} className="text-darkBlue/70 absolute text-[3rem] bottom-16 left-[20%]"/>
                 </div>
             </div>
         </div>
