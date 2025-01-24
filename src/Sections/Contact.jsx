d3a4a6b31693ad910ce19c698e9ce9a66241f256
danger-3003 <naremsumanth@gmail.com>
2025-01-24 16:45:29 +0530

    Updated Contact form

--- src/Sections/Contact.jsx
+++ src/Sections/Contact.jsx
@@ -1,9 +1,274 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
+import Container from "../Components/Container";
+import axios from "axios";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faLocationDot, faEnvelope, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+import "../Components/loader.css";
 
 function Contact() {
-  return (
-    <div>Contact</div>
-  )
+    const contactSection = useRef(null);
+    const location = useLocation();
+    const [details, setDetails] = useState({
+        name: "",
+        email: "",
+        number: "",
+        Subject: "",
+        Body: "",
+    });
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (location.hash === "#contact" && contactSection.current) {
+            contactSection.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [location]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+        const data = {
+            name: details.name,
+            email: details.email,
+            toMail: "srkengineering@yahoo.co.in",
+            toName: "Sree Rama Krishna Engineering Company",
+            phone: details.number,
+            subject: details.Subject, // Include inquiryType in subject
+            message: details.Body,
+        };
+        axios
+            .post("https://api.qrdcard.com/api/url/sendmail", data)
+            .then((res) => {
+                if (res) {
+                    alert("Form Submitted Successfully");
+                    setDetails({
+                        details,
+                        name: "",
+                        email: "",
+                        number: "",
+                        Subject: "",
+                        Body: "",
+                    });
+                    setLoading(false);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        console.log(details);
+    };
+
+    return (
+        <div
+            ref={contactSection}
+            className="font-mplus flex items-cener justify-center pt-28 pb-20 sm:px-5 md:px-10"
+        >
+            <div className="w-full lg:w-[50rem] flex items-center justify-center ">    
+                <div className="bg-secondary w-full flex items-center justify-between flex-col sm:flex-row gap-5 p-5 py-10 rounded-xl shadow-customShadow">
+                    <div className="ml-5 sm:w-[17rem] lg:w-[20rem] xl:w-auto">
+                        <p className="font-bold text-4xl sm:text-3xl lg:text-4xl text-darkBlue mb-5">
+                            Want to know more?
+                        </p>
+                        <p className="text-xl">
+                            Reach out to us for inquiries, bookings, or more
+                            details!
+                        </p>
+                        <div className="w-60 sm:w-[18rem] mt-5">
+                            <div className="font-light">
+                                <div className="flex items-start justify-start gap-5">
+                                    <FontAwesomeIcon
+                                        icon={faLocationDot}
+                                        className="mt-1"
+                                    />
+                                    <a
+                                        href="https://maps.app.goo.gl/5rFq3T8XXKWhSMgq9"
+                                        target="_blank"
+                                    >
+                                        Port Stadium, Akkayyapalem Main Rd,
+                                        Kailasapuram, Visakhapatnam, Andhra
+                                        Pradesh 530016
+                                    </a>
+                                </div>
+                                <div className="flex items-start justify-start gap-5">
+                                    <FontAwesomeIcon
+                                        icon={faMobileAlt}
+                                        className="mt-1"
+                                    />
+                                    <a href="tel:++91 8297667878">
+                                        +91 8297667878
+                                    </a>
+                                </div>
+                                <div className="flex items-start justify-start gap-5">
+                                    <FontAwesomeIcon
+                                        icon={faEnvelope}
+                                        className="mt-1"
+                                    />
+                                    <a
+                                        href="mailto:vishwanadhsportsclub@gmail.com"
+                                        className="max-w-52"
+                                    >
+                                        vishwanadhsportsclub @gmail.com
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`bg-text px-5 ${window.innerWidth < 350?"w-full":"w-72"} sm:w-[50%] lg:px-10 py-5 rounded-xl`}>
+                        <form action="" onSubmit={handleSubmit}>
+                            <div
+                                className={`${
+                                    window.innerWidth < 350
+                                        ? "w-full"
+                                        : "w-full"
+                                } sm:w-[100%] lg:w-72 my-2`}
+                            >
+                                <p className="font-semibold text-lg">
+                                    Name <span className="text-red-600">*</span>
+                                </p>
+                                <input
+                                    type="text"
+                                    placeholder="Your Name"
+                                    value={details.name}
+                                    required
+                                    name="name"
+                                    onChange={(e) => {
+                                        setDetails({
+                                            ...details,
+                                            name: e.target.value,
+                                        });
+                                    }}
+                                    className="border border-primary outline-none bg-white shadow-md shadow-slate-400 rounded-lg w-full px-2 py-1 text-slate-900"
+                                />
+                            </div>
+                            <div
+                                className={`${
+                                    window.innerWidth < 350
+                                        ? "w-full"
+                                        : "w-full"
+                                } sm:w-[100%] lg:w-72 my-2`}
+                            >
+                                <p className="font-semibold text-lg">
+                                    Email{" "}
+                                    <span className="text-red-600">*</span>
+                                </p>
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    value={details.email}
+                                    required
+                                    name="email"
+                                    onChange={(e) => {
+                                        setDetails({
+                                            ...details,
+                                            email: e.target.value,
+                                        });
+                                    }}
+                                    className="border border-primary outline-none bg-white shadow-md shadow-slate-400 rounded-lg w-full px-2 py-1 text-slate-900"
+                                />
+                            </div>
+                            <div
+                                className={`${
+                                    window.innerWidth < 350
+                                        ? "w-full"
+                                        : "w-full"
+                                } sm:w-[100%] lg:w-72 my-2`}
+                            >
+                                <p className="font-semibold text-lg">
+                                    Mobile Number{" "}
+                                    <span className="text-red-600">*</span>
+                                </p>
+                                <input
+                                    type="number"
+                                    placeholder="Your mobile number"
+                                    value={details.number}
+                                    required
+                                    name="mobile"
+                                    onChange={(e) => {
+                                        setDetails({
+                                            ...details,
+                                            number: e.target.value,
+                                        });
+                                    }}
+                                    className="border border-primary outline-none bg-white shadow-md shadow-slate-400 rounded-lg w-full px-2 py-1 text-slate-900"
+                                />
+                            </div>
+                            <div
+                                className={`${
+                                    window.innerWidth < 350
+                                        ? "w-full"
+                                        : "w-full"
+                                } sm:w-[100%] lg:w-72 my-2`}
+                            >
+                                <p className="font-semibold text-lg">
+                                    Subject
+                                    <span className="text-red-600">*</span>
+                                </p>
+                                <input
+                                    type="text"
+                                    placeholder="Subject"
+                                    required
+                                    value={details.Subject}
+                                    name="country"
+                                    onChange={(e) => {
+                                        setDetails({
+                                            ...details,
+                                            Subject: e.target.value,
+                                        });
+                                    }}
+                                    className="border border-primary outline-none bg-white shadow-md shadow-slate-400 rounded-lg w-full px-2 py-1 text-slate-900"
+                                />
+                            </div>
+                            <div
+                                className={`${
+                                    window.innerWidth < 350
+                                        ? "w-full"
+                                        : "w-full"
+                                } sm:w-[100%] lg:w-72 my-2`}
+                            >
+                                <p className="font-semibold text-lg">
+                                    Message
+                                    <span className="text-red-600">*</span>
+                                </p>
+                                <input
+                                    type="text"
+                                    placeholder="Your Message"
+                                    name="body"
+                                    value={details.Body}
+                                    onChange={(e) => {
+                                        setDetails({
+                                            ...details,
+                                            Body: e.target.value,
+                                        });
+                                    }}
+                                    className="border border-primary outline-none bg-white shadow-md shadow-slate-400 rounded-lg w-full px-2 py-1 text-slate-900"
+                                />
+                            </div>
+                            <div className="flex justify-center mt-5">
+                                {
+                                    loading?
+                                    <div className="w-20 h-8 rounded bg-darkBlue flex items-center justify-center shadow-md shadow-slate-400">
+                                        <div
+                                            className="loader"
+                                        />
+                                    </div>
+                                    :
+                                    <button
+                                        type="submit"
+                                        className="bg-darkBlue rounded-sm px-6 py-1 text-[#fff] font-semibold shadow-md shadow-slate-400 transition-all duration-300"
+                                    >
+                                        Submit
+                                    </button>
+                                }
+                                
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Contact
+export default Contact;
--- src/Sections/FAQ.jsx
+++ src/Sections/FAQ.jsx
@@ -1,9 +1,171 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Container from "../Components/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 function FAQ() {
-  return (
-    <div className='bg-darkBlue'>FAQ</div>
-  )
+    const [state, setState] = React.useState("");
+    const location = useLocation();
+    const faqRef = React.useRef(null);
+
+    const handleState = (questionNumber) => {
+        setState((prev)=> prev === questionNumber ? "" : questionNumber);
+        console.log(state);
+    };
+
+    React.useEffect(() => {
+        if (location.hash === "#FAQ" && faqRef.current) {
+            faqRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [location]);
+
+    return (
+        <div
+            ref={faqRef}
+            className="bg-darkBlue font-mplus flex items-center justify-center px-5 sm:px-10 pt-28 pb-20"
+        >
+            <Container>
+                <div>
+                    <div className="hidden">
+                        <input
+                            type="radio"
+                            name="question"
+                            id="one"
+                            onClick={() => {
+                                handleState("one");
+                            }}
+                        />
+                        <input
+                            type="radio"
+                            name="question"
+                            id="two"
+                            onClick={() => {
+                                handleState("two");
+                            }}
+                        />
+                        <input
+                            type="radio"
+                            name="question"
+                            id="three"
+                            onClick={() => {
+                                handleState("three");
+                            }}
+                        />
+                    </div>
+                    <div className="mb-10 text-center">
+                        <p className="font-semibold text-yellow text-3xl md:text-4xl">FAQ's</p>
+                    </div>
+                    <div>
+                        <div>
+                            <label
+                                htmlFor="one"
+                                className={`cursor-pointer py-3 ${
+                                    state === "one"
+                                        ? "bg-yellow font-semibold"
+                                        : "bg-text font-medium"
+                                } text-primary px-5 rounded-md flex items-center justify-between transition-all duration-300`}
+                            >
+                                <div>
+                                    What are the operating hours of Vishwanadh
+                                    Sports Club?
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon={faChevronDown}
+                                        className={`${
+                                            state === "one"
+                                                ? "-rotate-180"
+                                                : "rotate-0"
+                                        } transition-all duration-300`}
+                                    />
+                                </div>
+                            </label>
+                            <div
+                                className={`overflow-hidden px-5 ${
+                                    state === "one"
+                                        ? "max-h-96 py-5"
+                                        : "max-h-0 py-0"
+                                } transition-all duration-300 text-text font-extralight`}
+                            >
+                                We are open from 9:00 AM to 9:00 PM, seven days
+                                a week.
+                            </div>
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="two"
+                                className={`cursor-pointer py-3 mt-5 ${
+                                    state === "two"
+                                        ? "bg-yellow font-semibold"
+                                        : "bg-text font-medium"
+                                } text-primary px-5 rounded-md flex items-center justify-between transition-all duration-300`}
+                            >
+                                <div>
+                                    Is pre-booking required for adventure rides?
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon={faChevronDown}
+                                        className={`${
+                                            state === "two"
+                                                ? "-rotate-180"
+                                                : "rotate-0"
+                                        } transition-all duration-300`}
+                                    />
+                                </div>
+                            </label>
+                            <div
+                                className={`overflow-hidden px-5 ${
+                                    state === "two"
+                                        ? "max-h-96 py-5"
+                                        : "max-h-0 py-0"
+                                } transition-all duration-300 text-text font-extralight`}
+                            >
+                                Pre-booking is recommended for weekends and
+                                holidays to avoid queues.
+                            </div>
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="three"
+                                className={`cursor-pointer py-3 mt-5 ${
+                                    state === "three"
+                                        ? "bg-yellow font-semibold"
+                                        : "bg-text font-medium"
+                                } text-primary px-5 rounded-md flex items-center justify-between transition-all duration-300`}
+                            >
+                                <div>
+                                    Are there facilities for kids at the club?
+                                </div>
+                                <div>
+                                    <FontAwesomeIcon
+                                        icon={faChevronDown}
+                                        className={`${
+                                            state === "three"
+                                                ? "-rotate-180"
+                                                : "rotate-0"
+                                        } transition-all duration-300`}
+                                    />
+                                </div>
+                            </label>
+                            <div
+                                className={`overflow-hidden px-5 ${
+                                    state === "three"
+                                        ? "max-h-96 py-5"
+                                        : "max-h-0 py-0"
+                                } transition-all duration-300 text-text font-extralight`}
+                            >
+                                Absolutely! We offer a variety of kid-friendly
+                                activities, including soft play areas, rope
+                                courses, and a snow zone.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
 }
 
-export default FAQ
+export default FAQ;
